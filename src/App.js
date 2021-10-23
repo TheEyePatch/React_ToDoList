15784f1a647321dade823a27e11c2d33332e29bd
Leif Bersamina <leifbersamina@gmail.com>
2021-10-23 23:05:03 +0800

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+import CategoriesPage from './pages/CategoriesPage';
+import Task from './components/Task'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <NavBar/>
+    <Switch>
+      <Route path='/categories' exact>
+        <CategoriesPage/>
+      </Route>
+    </Switch>
+    
+   </div>
   );
 }
 
