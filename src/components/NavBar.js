d73df86f97072bee38760fa9e2568dc9da59dee1
Leif Bersamina <leifbersamina@gmail.com>
2021-11-01 22:58:10 +0800

    add nav bar links and basic styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+import classes from '../styles/components/NavBar.module.css'
 
 function NavBar(){
   return (
-    <nav>
-      This is Nav
+    <nav className={classes.nav}>
+      <ul>
+        <li>
+        <Link to='/'>Home</Link>
+        </li>
+        <li>
+        <Link to='/categories'>Categories</Link>
+        </li>
+        <li>
+          <Link to='/login'>Logout</Link>
+        </li>
+      </ul>
     </nav>
   )
 };
